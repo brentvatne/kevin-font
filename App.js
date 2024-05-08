497eedc1bb6b191eee717546268f679a8fbef849
Brent Vatne <brentvatne@gmail.com>
2024-05-08 16:50:18 -0700

    Example

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Ubuntu-Light'}}>Ubuntu Light</Text>
+      <Text style={{ fontFamily: 'Ubuntu-Bold'}}>Ubuntu Bold</Text>
       <StatusBar style="auto" />
     </View>
   );
